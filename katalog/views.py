67fe5736c421397c2502afafbc9c991efa7de934
unknown <rafiadiwibowo9@gmail.com>
2022-09-11 17:57:17 +0700

    tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_barang_katalog,
+        'nama' : 'Muhammad Rafi Adiwibowo',
+        'id' : '2106653855',
+    }
+    return render(request, "katalog.html", context)
+
