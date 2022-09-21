68eacc7ff16dfa9f8fd5c9e4e216125d5011034f
unknown <rafiadiwibowo9@gmail.com>
2022-09-21 15:23:07 +0700

    update tugas 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,7 +1,19 @@
+from pickle import TRUE
 from django.shortcuts import render
 from mywatchlist.models import Film
 from django.http import HttpResponse
 from django.core import serializers
+
+def status_film(request):
+    data_film = Film.objects.all()
+    true = 0
+    for i in data_film:
+        if i.watched == True:
+            true += 1
+    if true >= len(data_film) - true:
+        return "Selamat, kamu sudah banyak menonton!"
+    else :
+        return "Wah, kamu masih sedikit menonton!"
 
 # Create your views here.
 def show_film(request):
@@ -10,6 +22,7 @@ def show_film(request):
         'list_film' : data_film,
         'nama' : 'Muhammad Rafi Adiwibowo',
         'id' : '2106653855',
+        'info' : status_film(request),
     }
     return render(request, "mywatchlist.html", context)
 
