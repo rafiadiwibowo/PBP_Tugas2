e82f0fa8cc266f458bd58cfd3f3e8111e0a39c9d
unknown <rafiadiwibowo9@gmail.com>
2022-09-28 14:35:07 +0700

    update tugas 4

--- project_django/settings.py
+++ project_django/settings.py
@@ -28,7 +28,7 @@ PRODUCTION = os.getenv('DATABASE_URL') is not None
 SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
