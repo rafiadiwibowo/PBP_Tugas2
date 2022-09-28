6dc80a277232c324b11a2962ce0808656df5b181
unknown <rafiadiwibowo9@gmail.com>
2022-09-28 14:52:19 +0700

    update tugas 4

--- todolist/urls.py
+++ todolist/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('create-task/', create_task, name='create-task'),
     path('logout/', logout_user, name='logout'),
-    path('todolist', show_todolist, name=show_todolist),
+    path('todolist/', show_todolist, name=show_todolist),
 ]
