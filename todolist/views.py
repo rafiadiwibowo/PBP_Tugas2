1efc4309c45af829629dba2023971ea56215558c
unknown <rafiadiwibowo9@gmail.com>
2022-09-28 13:54:10 +0700

    update tugas 4

--- todolist/views.py
+++ todolist/views.py
@@ -51,12 +51,14 @@ def login_user(request):
     context = {}
     return render(request, 'login.html', context)
 
+@login_required(login_url='/todolist/login/')
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('todolist:login'))
     response.delete_cookie('last_login')
     return response
 
+@login_required(login_url='/todolist/login/')
 def create_task(request):
     if request.method == 'POST':
         form = FormTask(request.POST)
